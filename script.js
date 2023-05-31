eeef21d58d4a840dd88d28102d74b4c9fa38d462
User <user@Andys-iMac.lan>
2023-05-30 20:11:04 -0700

    updated read me

--- script.js
+++ script.js
@@ -3,22 +3,29 @@ let searchBtn = document.querySelector("#searchBtn");
 let container = document.querySelector(".container");
 let img = document.querySelector(".img");
 let info = document.querySelector(".info");
-// console.log (searchInput);
-// console.dir(searchInput);
+let cards = document.querySelector(".cards");
+
+
+const movies = ["Iron Man", "Avengers", "Guardians of the Galaxy"];
+
 let baseApiUrl = "https://www.omdbapi.com/?apikey=4e32e49b&t=";
 
 function searchFunc() {
+    info.innerHTML = "";
+    img.innerHTML = "";
+
     let apiUrl = baseApiUrl + searchInput.value;
-    // alert(searchInput.value);
 
 fetch(apiUrl).then(res => res.json()) .then(data => {
+
     let poster = document.createElement("img");
     poster.src = data.Poster;
+
     
     let title = document.createElement("p");
+    title.textContent = data.Title;
 
     let year = document.createElement("p");
-    title.textContent = data.Title;
     year.textContent = data.Year;
 
     let rated = document.createElement("p");
@@ -33,16 +40,56 @@ fetch(apiUrl).then(res => res.json()) .then(data => {
     img.appendChild(poster);
     info.appendChild(title, year, rated, released, runtime);
     container.append(poster, title, year)
-})
+});
+searchInput.value = "";
 }
 
 function enterKeyFunc(e) {
     if (e.keyCode == 13)
     searchFunc();
 }
-
+//click event
 searchBtn.addEventListener("click", searchFunc);
-
+//enter key event
 window.addEventListener("keypress", (e) => {
     enterKeyFunc(e);
 });
+
+function defaultSearch(arr) {
+    arr = movies;
+    arr.map(movie => {
+        fetch(`${baseApiUrl} + ${movie}`).then(res => res.json()).then(data => {
+            console.log(data);
+
+            let cardDiv = document.createElement("div");
+            cardDiv.classList.add("card");
+
+            let imgEl = document.createElement("img");
+            imgEl.classList.add("card-img-top");
+            imgEl.src = data.Poster;
+            imgEl.style.width = "200px";
+
+            let cardBodyDiv = document.createElement("div");
+            cardBodyDiv.classList.add("card-body");
+            
+            let cardTitle = document.createElement("h5");
+            cardTitle.classList.add("card-title");
+            cardTitle.textContent = data.Title;
+
+            let cardText = document.createElement("p");
+            cardText.classList.add("card-text");
+            cardText.textContent = data.Year;
+
+            cardBodyDiv.append(cardTitle, cardText);
+            cardDiv.append(imgEl, cardBodyDiv);
+            cards.appendChild(cardDiv);
+
+        })
+    })
+}
+
+//dom content loaded
+window.addEventListener("DOMContentLoaded", defaultSearch);
+window.addEventListener("load", () => {
+    searchInput.focus();
+})
